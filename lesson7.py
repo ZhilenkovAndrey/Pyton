eae1ed52714f192396eca2ccd76f879292fab51e
ZhilenkovAndrey <dron19812013@gmail.com>
2023-06-18 15:57:14 +0300

    12

--- lesson7.py
+++ lesson7.py
@@ -12,15 +12,42 @@
 #    <function_name>("Трам-пара-папам-парам-па-пам-пам-па Пум-пурум-пу-пурум-трам-пам-па") -> False
 #    <function_name>("Пам-парам-пурум Пум-пурум-карам") -> True
 
-def rhythm(str):
-    str = str.split()
-    list_1 = []
-    for word in str:
-        sum_w = 0
-        for i in word:
-            if i in 'аеёиоуыэюя':
-                sum_w += 1
-        list_1.append(sum_w)
-    return len(list_1) == list_1.count(list_1[0])
+#def rhythm(str):
+#    str = str.split()
+#    list_1 = []
+#    for word in str:
+#        sum_w = 0
+#        for i in word:
+#            if i in 'аеёиоуыэюя':
+#                sum_w += 1
+#        list_1.append(sum_w)
+#    return len(list_1) == list_1.count(list_1[0])
 
-print(rhythm(input('введите строку: ')))
+#print(rhythm(input('введите строку: ')))
+
+
+#6.2[32]: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая 
+#принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца, т.е. 
+#функцию двух аргументов. Аргументы num_rows и num_columns указывают число строк и столбцов 
+#таблицы, которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы.
+#Примеры/Тесты:
+#print_operation_table(lambda x,y: x**y,4,4)
+#1   1   1   1
+#2   4   8  16
+#3   9  27  81
+#4  16  64 256
+#print_operation_table(lambda x,y: x*y)
+#1   2   3   4   5   6
+#2   4   6   8  10  12
+#3   6   9  12  15  18
+#4   8  12  16  20  24
+#5  10  15  20  25  30
+#6  12  18  24  30  36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x ** y, 4, 4)
+print_operation_table(lambda x,y: x*y)
